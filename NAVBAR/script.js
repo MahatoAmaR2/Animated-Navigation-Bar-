7db02a4366eea1e8f3eeddc3c244efe55c0618c0
MahatoAmaR2 <bauaa476@gmail.com>
2025-06-29 15:31:36 +0530

    add style

--- NAVBAR/script.js
+++ NAVBAR/script.js
@@ -1,1 +1,6 @@
-console.log("Har Har Mahadev");
+const searchToggle = document.getElementById("searchToggle");
+const searchWrapper = document.getElementById("searchWrapper");
+
+searchToggle.addEventListener("click", () => {
+  searchWrapper.classList.toggle("active");
+});
