3ace08c4fc2afec239f8e2e8a03cebcfcbab4d7e
MahatoAmaR2 <bauaa476@gmail.com>
2025-07-01 20:18:00 +0530

    some script and css changes

--- NAVBAR/script.js
+++ NAVBAR/script.js
@@ -5,5 +5,23 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navLinks.classList.toggle("active");
   console.log("HAR HAR MAHADEV");
-  
+});
+
+const links = document.querySelectorAll(".nav-links a");
+const indicator = document.getElementById("indicator");
+
+function resetIndicatorClasses() {
+  for (let i = 1; i <= 5; i++) {
+    indicator.classList.remove(`position-${i}`);
+  }
+}
+
+links.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    links.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
+
+    resetIndicatorClasses();
+    indicator.classList.add(`position-${index + 1}`);
+  });
 });
